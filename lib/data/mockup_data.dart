2d4f718f87499ecbe35d1007ed72873dc9d481c9
fjnkhewgkfncsd <Mengly.heng@student.cadt.edu.kh>
2026-04-12 19:13:58 +0700

    removed unnecessary file and created fake data

---
+++ lib/data/mockup_data.dart
@@ -0,0 +1,93 @@
+Map<String, dynamic> stations = {
+  "001": {
+    "name": "monich is gay as fuck",
+    "availableBike": 5,
+    "totalSlots": 10,
+  },
+  "002": {
+    "name": "monich is gay as fuck",
+    "availableBike": 5,
+    "totalSlots": 10,
+  },
+  "003": {
+    "name": "monich is gay as fuck",
+    "availableBike": 5,
+    "totalSlots": 10,
+  },
+  "004": {
+    "name": "monich is gay as fuck",
+    "availableBike": 5,
+    "totalSlots": 10,
+  },
+  "005": {
+    "name": "monich is gay as fuck",
+    "availableBike": 5,
+    "totalSlots": 10,
+  },
+};
+
+Map<String, dynamic> slots = {
+  "1": {"isOccupied": true, "bikeId": "001"},
+  "2": {"isOccupied": true, "bikeId": "001"},
+  "3": {"isOccupied": true, "bikeId": "001"},
+  "4": {"isOccupied": true, "bikeId": "001"},
+  "5": {"isOccupied": true, "bikeId": "001"},
+  "6": {"isOccupied": false},
+  "7": {"isOccupied": false},
+  "8": {"isOccupied": false},
+  "9": {"isOccupied": false},
+  "10": {"isOccupied": false},
+};
+
+Map<String, dynamic> bikes = {
+  "001": {
+    "bikeNumber": "Bike Number #001",
+    "status": "available",
+  },
+  "002": {
+    "bikeNumber": "Bike Number #001",
+    "status": "available",
+  },
+  "003": {
+    "bikeNumber": "Bike Number #001",
+    "status": "available",
+  },
+  "004": {
+    "bikeNumber": "Bike Number #001",
+    "status": "available",
+  },
+  "005": {
+    "bikeNumber": "Bike Number #001",
+    "status": "available",
+  },
+  "006": {
+    "bikeNumber": "Bike Number #001",
+    "status": "available",
+  },
+  "007": {
+    "bikeNumber": "Bike Number #001",
+    "status": "available",
+  },
+  "008": {
+    "bikeNumber": "Bike Number #001",
+    "status": "available",
+  },
+};
+
+Map<String, dynamic> passes = {
+  "001": {
+    "pass_type": "day",
+    "price": 10.0,
+    "description": "kim heng is gay as well",
+  },
+  "002": {
+    "pass_type": "monthly",
+    "price": 10.0,
+    "description": "monich is gay",
+  },
+  "003": {
+    "pass_type": "annumual",
+    "price": 10.0,
+    "description": "monich is gay",
+  }
+};
--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'main_common.dart';
-
-void main() {
-  
-}
