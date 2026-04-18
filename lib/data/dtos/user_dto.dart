a91b5bbc59e11a816674f33dedaba645d8142d70
fjnkhewgkfncsd <Mengly.heng@student.cadt.edu.kh>
2026-04-18 17:35:07 +0700

    update user dto

--- lib/data/dtos/user_dto.dart
+++ lib/data/dtos/user_dto.dart
@@ -4,14 +4,14 @@ class UserDto{
   static const String idKey = 'id';
   static const String emailKey = 'email';
   static const String usernameKey = 'username';
-  static const String passStatusKey = 'pass_status';
+  static const String passStatusKey = 'isPassActive';
   static const String passTypeKey = 'pass_type';
   static const String passExpiryDateKey = 'pass_expiry_date';
 
   static User fromJson(String id, Map<String, dynamic> json) {
     assert(json[emailKey] is String);
     assert(json[usernameKey] is String);
-    assert(json[passStatusKey] is String);
+    assert(json[passStatusKey] is bool);
     assert(json[passTypeKey] == null || json[passTypeKey] is String);
     assert(json[passExpiryDateKey] == null || json[passExpiryDateKey] is DateTime);
 
@@ -19,7 +19,7 @@ class UserDto{
       id: id,
       email: json[emailKey],
       username: json[usernameKey],
-      passStatus: json[passStatusKey],
+      isPassActive: json[passStatusKey],
       passType: json[passTypeKey],
       passExpiryDate: json[passExpiryDateKey] 
     );
