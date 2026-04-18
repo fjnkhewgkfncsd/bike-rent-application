92dfcc1d3905e2854aadbccb0c47cd0bb9b3c679
fjnkhewgkfncsd <Mengly.heng@student.cadt.edu.kh>
2026-04-18 17:49:45 +0700

    update pass screeen to spp dynamix app title

---
+++ lib/ui/screens/passes/passes_screen.dart
@@ -0,0 +1,64 @@
+import 'package:bike_rental/data/repositories/pass/pass_repository.dart';
+import 'package:bike_rental/ui/screens/passes/view_model/passes_view_model.dart';
+import 'package:bike_rental/ui/screens/passes/widgets/passes_content.dart';
+import 'package:bike_rental/ui/state/user_state.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PassesScreen extends StatelessWidget {
+  const PassesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => PassesViewModel(
+        userState: context.read<UserState>(),
+        passRepository: context.read<PassRepository>(),
+      ),
+      child: Builder(
+        builder: (context) {
+          final userState = context.watch<UserState>();
+          final user = userState.user;
+          final vm = context.watch<PassesViewModel>();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                user == null
+                    ? 'Loading...'
+                    : user.isPassActive
+                        ? 'Manage Your Pass'
+                        : 'Choose a Pass',
+              ),
+            ),
+            body: PassesContent(passesVM: vm),
+            bottomNavigationBar: (user == null || !user.isPassActive)
+                ? const SizedBox()
+                : SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: SizedBox(
+                        height: 55,
+                        width: double.infinity,
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            vm.cancelSubscription();
+                          },
+                          icon: const Icon(Icons.close),
+                          label: const Text('Cancel Plan'),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF2D63C8),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/passes/widgets/passes_content.dart
@@ -0,0 +1,170 @@
+import 'package:bike_rental/domain/model/pass/pass.dart';
+import 'package:bike_rental/ui/screens/pass/pass_details_screen.dart';
+import 'package:bike_rental/ui/screens/passes/view_model/passes_view_model.dart';
+import 'package:bike_rental/ui/widgets/generic_tile.dart';
+import 'package:flutter/material.dart';
+import '../../../utils/async_value.dart';
+
+class PassesContent extends StatelessWidget {
+  final PassesViewModel passesVM;
+  const PassesContent({super.key,required this.passesVM});
+
+  void onClickSubscription(Pass pass){
+    DateTime now = DateTime.now() ;
+    passesVM.userSubscriptPass(pass.passtype,now.add(Duration(days: 30)) );
+  }
+
+  void toPassDetails(BuildContext context,Pass pass) {
+    Navigator.push(context,
+      MaterialPageRoute(builder: (context) => PassDetailsScreen(pass: pass,onClickSubscription:() => onClickSubscription(pass))),
+    );
+  }
+
+  String getSubTitle(Pass pass){
+    switch(pass.passtype){
+      case 'day':
+        return '\$${pass.price} / day';
+      case 'monthly':
+        return '\$${pass.price} / month';
+      case 'annual':
+        return '\$${pass.price} / year';
+      default:
+        return '';
+    }
+  }
+
+  IconData getLeadingIcon(Pass pass){
+    switch(pass.passtype){
+      case 'day':
+        return Icons.calendar_today;
+      case 'monthly':
+        return Icons.calendar_view_month;
+      case 'annual':
+        return Icons.calendar_view_day_rounded;
+      default:
+        return Icons.calendar_today;
+    }
+  }
+
+  String getValidUntilText(DateTime expiryDate){
+    return '${expiryDate.day}/${expiryDate.month}/${expiryDate.year}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final state = passesVM.state;
+
+    Widget content;
+
+    switch(state.state){
+      case AsyncValueState.loading:
+        content = Center(child: CircularProgressIndicator());
+        break;
+      case AsyncValueState.error:
+        content = Center(child: Text(state.error != null ? 'There is something wrong': 'error'));
+        break;
+      case AsyncValueState.success:
+        if(passesVM.userState.user!.isPassActive){
+          content = Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+            child: Column(
+              children : [
+                ...passesVM.passes.map((pass){
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: GenericListTile(
+                      title: pass.passtype, 
+                      backgroundColor: pass.passtype == passesVM.userState.user!.passType ? Colors.blue : const Color.fromARGB(255, 170, 170, 170),
+                      subtitle: getSubTitle(pass),
+                      trailing: pass.passtype == passesVM.userState.user!.passType ? 
+                      Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8)
+                        ),
+                        child: Text('Active', style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),),
+                      ) : null,
+                      leading: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.lightBlueAccent,
+                        ),
+                        child: Icon(getLeadingIcon(pass))),
+                    ),
+                  );}
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 22),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF3F3F3),
+                    borderRadius: BorderRadius.circular(28),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Current Subscription',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF1F1F1F),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Valid Until',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFF8A8A8A),
+                            ),
+                          ),
+                          Text(
+                            getValidUntilText(passesVM.userState.user!.passExpiryDate!),
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xFF1F1F1F),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ]
+            )
+          );
+        }else{
+          content = Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                final pass = passesVM.passes[index];
+                return GenericListTile(
+                  title: pass.passtype,
+                  trailing: Text('${pass.price} €'),
+                  backgroundColor: Colors.blue,
+                  onTap: () => toPassDetails(context, pass),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 16);
+              },
+              itemCount: passesVM.passes.length,
+            ),
+          );
+          break;
+        }
+        
+    }
+    return content;
+  }
+}
