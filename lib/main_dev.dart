0d24df988ef4f57b6916205cece8788bb5c1534c
fjnkhewgkfncsd <Mengly.heng@student.cadt.edu.kh>
2026-04-18 17:43:47 +0700

    injected user state and other repo in main dev

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,3 +1,4 @@
+import 'package:bike_rental/ui/state/user_state.dart';
 import 'data/repositories/bike/bike_repository.dart';
 import 'data/repositories/bike/bike_repository_mockup.dart';
 import 'data/repositories/pass/pass_repository.dart';
@@ -8,23 +9,35 @@ import 'data/repositories/station/station_repository.dart';
 import 'data/repositories/user/user_repository.dart';
 import 'data/repositories/user/user_repository_mockup.dart';
 import 'data/repositories/station/station_repository_mockup.dart';
-import 'domain/model/slot/slot.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import './main_common.dart';
 
 List<SingleChildWidget> get providers {
-
   return [
-    Provider<UserRepository>(create : (_) => UserRepositoryMockup()),
-    Provider<PassRepository>(create : (_) => PassRepositoryMockup()),
-    Provider<BikeRepository>(create : (_) => BikeRepositoryMockup()),
-    Provider<StationRepository>(create : (_) => StationRepositoryMockup()),
-    Provider<SlotRepository>(create : (_) => SlotRepositoryMockup()),
+    Provider<UserRepository>(
+      create: (_) => UserRepositoryMockup(),
+    ),
+    Provider<PassRepository>(
+      create: (_) => PassRepositoryMockup(),
+    ),
+    Provider<BikeRepository>(
+      create: (_) => BikeRepositoryMockup(),
+    ),
+    Provider<StationRepository>(
+      create: (_) => StationRepositoryMockup(),
+    ),
+    Provider<SlotRepository>(
+      create: (_) => SlotRepositoryMockup(),
+    ),
+    ChangeNotifierProvider(
+      create: (context) => UserState(
+        userRepository: context.read<UserRepository>(),
+      )..loadUser('001'),
+    ),
   ];
 }
 
-void main(){
+void main() {
   mainCommon(providers);
 }
