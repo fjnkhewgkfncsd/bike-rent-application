d12f0284069e733017f9d53bf7538a9fd3591035
fjnkhewgkfncsd <Mengly.heng@student.cadt.edu.kh>
2026-04-19 16:13:10 +0700

    changed from mockup repo to firebase repo

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,14 +1,14 @@
+import 'package:bike_rental/data/repositories/pass/pass_firebase_repository.dart';
 import 'package:bike_rental/data/repositories/slot/slot_firebase_repository.dart';
 import 'package:bike_rental/data/repositories/station/station_firebase_repository.dart';
+import 'package:bike_rental/data/repositories/user/user_firebase_repository.dart';
 import 'package:bike_rental/ui/state/user_state.dart';
 import 'data/repositories/bike/bike_repository.dart';
 import 'data/repositories/bike/bike_repository_mockup.dart';
 import 'data/repositories/pass/pass_repository.dart';
-import 'data/repositories/pass/pass_repository_mockup.dart';
 import 'data/repositories/slot/slot_repository.dart';
 import 'data/repositories/station/station_repository.dart';
 import 'data/repositories/user/user_repository.dart';
-import 'data/repositories/user/user_repository_mockup.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import './main_common.dart';
@@ -20,10 +20,10 @@ List<SingleChildWidget> get providers {
       create: (_) => FirebaseDatabase.instance,
     ),
     Provider<UserRepository>(
-      create: (_) => UserRepositoryMockup(),
+      create: (context) => UserFirebaseRepository(context.read<FirebaseDatabase>()),
     ),
     Provider<PassRepository>(
-      create: (_) => PassRepositoryMockup(),
+      create: (context) => PassFirebaseRepository(context.read<FirebaseDatabase>()),
     ),
     Provider<BikeRepository>(
       create: (_) => BikeRepositoryMockup(),
